cd157c933920a4edc1800441a41f48272d2dd281
javiergs <3814755+javiergs@users.noreply.github.com>
2026-05-19 21:42:48 -0700

    initialization

--- rebuild-vm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-cp ../COMPILER/target/COMPILER-1.0-SNAPSHOT.jar libs/pcode-vm-1.0-snapshot.jar
-rm -f libs/pcode-vm-1.0-obfuscated.jar
-./gradlew obfuscateJar
-jar tf libs/pcode-vm-1.0-obfuscated.jar | grep javiergs/vm
-./gradlew clean runIde
